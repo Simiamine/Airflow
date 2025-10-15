22b4cddbdc629ffb516f5553d10a51bab4317a15
Amine M'ZALI <mzaliamine@gmail.com>
2025-10-15 17:04:35 +0200

    Refactor stock market DAG to use sensor for API availability check and improve task definitions

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,18 +1,36 @@
 from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 @dag(
     start_date=datetime(2023, 1, 1),
-    schedule_interval='@daily',
+    schedule='@daily',
     catchup=False,
-    tags=['stock_market']
+    tags=['stock_market'],
+
 )
 def stock_market():
-
-    @task
-    def hello():
-        print("Stock market DAG is working ")
-
-    hello()
+    
+    @task.sensor(poke_interval=30, timeout=300, mode='poke')
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done=condition, xcom_value=url)
+    
+    get_stock_prices = PythonOperator(
+        task_id='get_stock_prices',
+        python_callable=_get_stock_prices,
+    )
+    
+    is_api_available()
+        
 
 stock_market()
+    
